7cec71c83b54ee5ee354eb5881a4e2123fc84f80
lucas boisgard <lucas.boisgard@epitech.eu>
2019-04-24 15:42:11 +0200

    add unitary test for class Deck

--- my_tcg/spec/deck.spec.js
+++ my_tcg/spec/deck.spec.js
@@ -1,9 +1,28 @@
-const Deck = require('../src/models/deck.js')
+import Deck from '../src/models/deck'
+const deck = new Deck({cards:[1, 2, 3, 3]})    
 
-describe("verifie la class deck", function () { 
-    var Deckcards = Deck.cards;
+describe("Deck.shuffle", function () { 
     it("checks si le deck a été mélanger", function() {
-        Deck.shuffle();
-        expect(Deck.cards).toEqual(DeckCards);
+        deck.shuffle()
+        expect([1, 2, 3, 3]).not.toBe(deck.cards);
+        
       });
+    it("check si shuffle return true", function () {
+        expect(true).toBe(deck.shuffle());
+    })
 })
+
+describe("Deck.draw", function () {
+    it("check le retour de draw()", function () {
+        expect(deck.cards[0]).toBe(deck.draw())
+    });
+    it('check la suppression de la carte tiré', function () {
+        expect(deck.cards.length).toBe(3)
+    })
+}); 
+
+describe("Deck.getCardsCount", function () {
+    it("check return getCardsCount", function () {
+        expect(deck.getCardsCount()).toBe(3);
+    });
+});
